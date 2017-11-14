3b5ab347954beb1ffd86705ff1f407f879808a8a
hong <hong@401PC-02>
2017-11-14 20:29:48 +0900

    메일보내기

---
+++ jjack/src/main/java/com/jjack/web/mail/MailVO.java
@@ -0,0 +1,32 @@
+package com.jjack.web.mail;
+
+public class MailVO {
+	Boolean yesno;
+	String name;
+	String date;
+	String nick;
+	public Boolean getYesno() {
+		return yesno;
+	}
+	public void setYesno(Boolean yesno) {
+		this.yesno = yesno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+}
---
+++ jjack/src/main/java/com/jjack/web/mail/jMail.java
@@ -0,0 +1,60 @@
+package com.jjack.web.mail;
+
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class jMail {
+	public static void sendMail(MailVO mvo) {
+		final String user = "jjakjjakchon@gmail.com"; //발신자의 이메일 아이디를 입력
+		final String password = "clapclap"; //발신자 이메일의 패스워드를 입력
+		InternetAddress from = new InternetAddress();
+		
+		// SMTP 서버 정보 설정
+		Properties prop = new Properties();
+		
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", 465);
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.ssl.enable", "true");
+		prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		
+		Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, password); 
+			}
+		});
+		try {
+            MimeMessage message = new MimeMessage(session);
+            String fromName = "짝 게스트하우스";
+            // 발신자 이름 한글화
+            try{
+                from = new InternetAddress(new String(fromName.getBytes("UTF-8"), "8859_1") + "<jjakjjakchon@gmail.com>");
+            }catch(UnsupportedEncodingException uee){
+                uee.printStackTrace();
+            }
+            // 이메일 발신자
+            message.setFrom(from);
+
+            //수신자메일주소
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress("sonullimi@gmail.com")); 
+
+            // Subject
+            message.setSubject("[짝 게스트하우스] 애정촌 입소를 신청해주셔서 감사합니다", "UTF-8"); //메일 제목을 입력
+
+            // Text
+            String mailHtml = MailForm.makeMail(mvo);
+            
+            message.setContent(mailHtml, "text/html; charset=UTF-8");    //메일 내용을 입력
+
+            // send the message
+            Transport.send(message); ////전송
+            System.out.println("message sent successfully...");
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+	}
+}
