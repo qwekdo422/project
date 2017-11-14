8ce4bc6ced2e25f0d41dd000c648d42080876bcb
daeo <daeo@daeo>
2017-11-14 15:07:32 +0900

    aa

---
+++ jjack/src/main/java/com/jjack/web/common/vo/HouseApplyVO.java
@@ -0,0 +1,7 @@
+package com.jjack.web.common.vo;
+
+public class HouseApplyVO {
+	String name;
+	String age;
+	String location;
+}
--- jjack/src/main/webapp/js/houseApply/calendar.js
+++ jjack/src/main/webapp/js/houseApply/calendar.js
@@ -1,82 +1,41 @@
-var date = new Date();
-/* 이벤트 날짜 등록 */
-var totalEvent = 
-	{
-		title:'20대 청년모임', 
-		start:'2017-11-11', 
-		end:'2017-11-13',
-		url: 'https://google.com/'
-	}
-$('#calendar').fullCalendar({
-	header : {
-		left : 'prev,next today',
-		center : 'title',
-		right : 'month,agendaWeek,agendaDay'
-	},
-	defaultDate : /*'2017-11-08'*/date.getTime(),
-	editable : false,
-	eventLimit : true, // allow "more" link when too many events
-	events : tEvent,
-	/*events : [ totalEvent ],*/
-/*
-$('#calendar').fullCalendar({
-	header : {
-		left : 'prev,next today',
-		center : 'title',
-		right : 'month,agendaWeek,agendaDay'
-	},
-	defaultDate : '2014-09-12',
-	editable : false,
-	eventLimit : true, // allow "more" link when too many events
-	events : [ {
-		title : 'Long Event',
-		start : '2014-09-07',
-		end : '2014-09-10',
-		url : 'https://google.com/'
-	}, {
-		id : 999,
-		title : 'Repeating Event',
-		start : '2014-09-09T16:00:00'
-	}, {
-		id : 999,
-		title : 'Repeating Event',
-		start : '2014-09-16T16:00:00'
-	}, {
-		title : 'Conference',
-		start : '2014-09-11',
-		end : '2014-09-13'
-	}, {
-		title : 'Meeting',
-		start : '2014-09-12T10:30:00',
-		end : '2014-09-12T12:30:00'
-	}, {
-		title : 'Lunch',
-		start : '2014-09-12T12:00:00'
-	}, {
-		title : 'Meeting',
-		start : '2014-09-12T14:30:00'
-	}, {
-		title : 'Happy Hour',
-		start : '2014-09-12T17:30:00'
-	}, {
-		title : 'Dinner',
-		start : '2014-09-12T20:00:00'
-	}, {
-		title : 'Birthday Party',
-		start : '2014-09-13T07:00:00'
-	} ],
-*/
-	eventClick : function(event) {
-		if (event.url) {
+$(document).ready(function() {
+	var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+	var date = new Date();
+	$('#calendar').fullCalendar({
+		header : {
+			left : 'prev,next today',
+			center : 'title',
+			right : 'month,agendaWeek,agendaDay'
+		},
+		defaultDate : /*'2017-11-08'*/date.getTime(),
+		editable : false,
+		eventLimit : true, // allow "more" link when too many events
+		events : tEvent,
+		eventClick : function(event) {
+			
+			alert("이벤트 클릭");
+			
+			/*
 			$.magnificPopup.open({
 				items : {
-					iframe : event.url,
 					type : 'iframe'
 				}
 
 			});
+			*/
+			
+			
+			//	월을 숫자형태로 바꾸위해 영문으로된 월을 추출
+			var inDay = event.start.toString().split(" ");
+			//	1번째 인덱스에서 영문 월을 추출해서 대문자로 변환
+			var month = inDay[1].toUpperCase();
+			for(var i=0; i < months.length; i++) {
+				if(month == months[i]) {
+					$("#inDay").val(inDay[3]+"-"+(i+1)+"-"+inDay[2]);
+				}
+			}
 		}
-	}
-
+	
+	});
 });
-
+	
