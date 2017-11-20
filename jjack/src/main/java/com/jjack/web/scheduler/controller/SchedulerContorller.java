c2cac732180f78dd611df84bbba58e6296560714
daeo <daeo@daeo>
2017-11-20 10:36:49 +0900

    일정관리

--- jjack/src/main/java/com/jjack/web/scheduler/controller/SchedulerContorller.java
+++ jjack/src/main/java/com/jjack/web/scheduler/controller/SchedulerContorller.java
@@ -32,7 +32,7 @@ public class SchedulerContorller {
 	public String scheduleList(String status, Model model) {
 		model.addAttribute("SCHEDULE", scService.getSchedule());
 		//	관리자 일정관리
-		if(status == "admin") {
+		if(status == "A") {
 			return "scheduler/list";
 		} else {
 			//	사용자 입소신청
